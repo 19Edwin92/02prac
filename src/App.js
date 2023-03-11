cbbd2606e15a2bdf851828b4a43b408c9841f3b1
19Edwin92 <b_a_s_e@naver.com>
2023-03-11 13:07:54 +0900

    리덕스툴킷+JSON+thunk 01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useState(null);
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    setInputValue('');
+  }
+
+  //    01 JONS.GET    ////////////////////////////////////////
+  const fechTodos = async () => {
+    const {data} = await axios.get('http://localhost:4001/todos');
+    setTodos(data);
+  }
+
+  useEffect(()=> {
+    fechTodos();
+  }, []);
+  console.log(todos)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+  <p>리덕스 툴킷으로 중앙상태관리소 제어하기</p>
+  <p>리덕스 툴킷 설치하기 : yarn add react-redux @reduxjs/toolkit</p>
+  <hr/>
+  <div>
+    <p>입력공간</p>
+    <form onSubmit={onSubmitHandler}>
+      <input type="text" value={inputValue} onChange={(e)=> setInputValue(e.target.value)}/>
+    </form>
+  </div>
+  <hr/>
+  <div>
+    <p>JSON 파일 읽어오기</p>
+    {todos?.map(el => (<div key={el.id}>{el.id} : {el.title}</div>))}
+  </div>
+
+  </>
+  )
 }
 
-export default App;
+export default App
